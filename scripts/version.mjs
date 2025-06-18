8d5d222650b29041845efa8f689c41d3ff6781d7
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2025-06-18 16:41:14 -0500

    aaaaaaa

--- scripts/version.mjs
+++ scripts/version.mjs
@@ -1,9 +1,9 @@
 import process from "node:child_process";
 import fs from "node:fs/promises";
-import manifestJson from "../src/manifest.json";
-import pkgJson from "../package.json";
+import moduleJson from "../src/module.json" with { type: "json" };
+import pkgJson from "../package.json" with { type: "json" };
 
-manifestJson.version = pkgJson.version;
-const output = JSON.stringify(manifestJson, undefined, 2);
-await fs.writeFile("src/manifest.json", output);
-process.execSync("git add manifest.json");
+moduleJson.version = pkgJson.version;
+const output = JSON.stringify(moduleJson, undefined, 2);
+await fs.writeFile("src/module.json", output);
+process.execSync("git add src/module.json");
